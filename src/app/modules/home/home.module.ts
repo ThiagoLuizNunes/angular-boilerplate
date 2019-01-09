2847729909429d1f909ed18ac92cf158687b8b7b
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-01-09 01:22:24 -0300

    import ui module in home module

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 
+import { UiModule} from '../ui/ui.module';
+
 @NgModule({
   declarations: [HomeComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    UiModule
+  ],
+  bootstrap: [HomeComponent]
 })
 export class HomeModule { }
