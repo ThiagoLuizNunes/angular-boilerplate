2ef55ef508ff22a64f548ebe188231acbae8df85
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-01-09 01:14:46 -0300

    export ui modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './ui/footer/footer.component';
-import { HeaderComponent } from './ui/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FooterComponent,
-    HeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
