293b04bb35e3fdf1e8abccd8e5808281403add2c
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-01-22 00:42:49 -0300

    add header and side bar layouts

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -4,10 +4,14 @@ import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { OverviewComponent } from './overview/overview.component';
 import { ProfileComponent } from './profile/profile.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
+    HeaderComponent,
+    SidebarComponent,
     OverviewComponent,
     ProfileComponent,
   ],
