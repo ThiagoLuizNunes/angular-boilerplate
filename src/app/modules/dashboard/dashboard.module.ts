1d6978befb251d137ac6d475ac06bdb44165a319
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-01-21 23:41:11 -0300

    create children routes in dashboard module

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
+import { OverviewComponent } from './overview/overview.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [
+    DashboardComponent,
+    OverviewComponent,
+    ProfileComponent,
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
   ]
 })
 export class DashboardModule { }
--- src/app/modules/dashboard/dashboard.routing.module.ts
+++ src/app/modules/dashboard/dashboard.routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './dashboard.component';
+import { OverviewComponent } from './overview/overview.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const dashboardRoutes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent, children: [
+    { path: 'overview', component: OverviewComponent },
+    { path: 'profile', component: ProfileComponent },
+  ]},
 ];
 
 @NgModule({
