dae5e308f91c304f099106d73372aed80b3c22f0
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-01-22 23:10:48 -0300

    create guards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-// import { HomeModule } from './modules/home/home.module';
-
-// import { LoginModule } from './modules/login/login.module';
-// import { LoginRoutingModule } from './modules/login/login.routing.module';
-
-// import { SignupModule } from './modules/signup/signup.module';
-// import { SignupRoutingModule } from './modules/signup/signup.routing.module';
-
-// import { DashboardRoutingModule } from './modules/dashboard/dashboard.routing.module';
-// import { DashboardModule } from './modules/dashboard/dashboard.module';
 
 @NgModule({
   declarations: [
@@ -22,13 +10,6 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    // LoginRoutingModule,
-    // SignupRoutingModule,
-    // DashboardRoutingModule,
-    // HomeModule,
-    // LoginModule,
-    // SignupModule,
-    // DashboardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/ui/ui.module.ts
+++ src/app/modules/ui/ui.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { UserService } from '../user/user.service';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
   imports: [
     CommonModule,
+    HttpClientModule,
     RouterModule
   ],
+  providers: [UserService],
   exports: [HeaderComponent, FooterComponent]
 })
 export class UiModule { }
