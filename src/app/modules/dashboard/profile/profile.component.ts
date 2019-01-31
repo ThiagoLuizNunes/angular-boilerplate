875683fa0865feb2ee6c720d53ff9addd7f63436
ThiagoLuizNunes <thiagoluiz.dev@gmail.com>
2019-01-31 20:59:41 +0000

    add validation to profile fields

--- src/app/modules/dashboard/profile/profile.component.ts
+++ src/app/modules/dashboard/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-profile',
@@ -8,17 +9,38 @@ import { AuthService } from '../../auth/auth.service';
 })
 export class ProfileComponent implements OnInit {
 
-  private user = {};
+  private user: any;
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private toastr: ToastrService,
+    private authService: AuthService
+    ) { }
 
   ngOnInit() {
     this.user = this.authService.getUser();
   }
 
   onSubmit(form) {
+    let empty;
+    const emailRegex = /\S+@\S+\.\S+/;
+    Object.entries(this.user).map(att => {
+      if (att[1] === undefined || att[1] === '') {
+        empty = true;
+      }
+    });
+    if (empty || !this.user.password || !this.user.confirm_password) {
+      this.toastr.error(`Complete all fields`);
+      return;
+    }
+    if (!this.user.email.match(emailRegex)) {
+      this.toastr.error(`Invalid email`);
+      return;
+    }
+    if (this.user.password !== this.user.confirm_password) {
+      this.toastr.error(`Password doesn't mach`);
+      return;
+    }
     console.log(this.user);
-    console.log(form);
+    console.log(form.value);
   }
-
 }
