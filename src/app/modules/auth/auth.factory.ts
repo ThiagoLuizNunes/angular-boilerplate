8f7a2c62d28a083eee3c27a955eca9e1ac6f3165
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-02-08 22:31:12 -0300

    update isAuthenticated method

--- src/app/modules/auth/auth.factory.ts
+++ src/app/modules/auth/auth.factory.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
+import { HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +10,22 @@ import { environment } from '../../../environments/environment';
 export class AuthFactory {
 
   constructor() { }
+
+  handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+  };
 
   getLocalStorage() {
     return JSON.parse(localStorage.getItem(environment.app_userkey));
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable, EventEmitter } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import ICallback from '../../shared/types/icallback.types';
 import { AuthFactory } from './auth.factory';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,11 +18,11 @@ export class AuthService {
 
   constructor(
     private http: HttpClient,
-    private strg: AuthFactory) { }
+    private authFactory: AuthFactory) { }
 
   getUser(): any {
     if (!this.user) {
-      this.user = this.strg.getLocalStorage();
+      this.user = this.authFactory.getLocalStorage();
     }
     return this.user;
   }
@@ -52,7 +54,7 @@ export class AuthService {
 
   logout(callback?: ICallback): any {
     this.user = null;
-    this.strg.removeLocalStorage();
+    this.authFactory.removeLocalStorage();
     if (callback) {
       callback(null);
     }
@@ -62,7 +64,7 @@ export class AuthService {
     this.http.patch<any>(`${this.api}/auth/user`, user)
       .subscribe(
         response => {
-          this.strg.setLocalStorage(response);
+          this.authFactory.setLocalStorage(response);
           if (callback) {
             callback(null, response);
           }
@@ -74,23 +76,19 @@ export class AuthService {
         }
       );
   }
+  getUserLicenses(id: any): Observable<any> {
+    return this.http.get<any>(`${this.api}/auth/user-licenses/${id}`)
+      .pipe(
+        retry(3), // retry a failed request up to 3 times
+        catchError(this.authFactory.handleError)
+      );
+  }
 
-  isAuthenticated(token: any, callback?: ICallback): any {
-    this.http.post<any>(`${this.api}/auth/validateToken`, { token })
-      .subscribe(
-        response => {
-          if (!response.valid) {
-            this.strg.removeLocalStorage();
-            callback(response);
-          } else {
-            callback(null, response);
-          }
-        },
-        error => {
-          if (callback) {
-            callback(error);
-          }
-        }
+  isAuthenticated(token: any): Observable<any> {
+    return this.http.post<any>(`${this.api}/auth/validateToken`, { token })
+      .pipe(
+        retry(3), // retry a failed request up to 3 times
+        catchError(this.authFactory.handleError)
       );
   }
 }
